3db16c529603e82ff1baf777db01dea70c343abb
titiscms <titisbackup@gmail.com>
2021-04-11 21:38:59 -0300

    Implementando permissões no authorities do token

--- src/main/java/com/algaworks/algafoog/auth/core/AuthUser.java
+++ src/main/java/com/algaworks/algafoog/auth/core/AuthUser.java
@@ -1,7 +1,8 @@
 package com.algaworks.algafoog.auth.core;
 
-import java.util.Collections;
+import java.util.Collection;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import com.algaworks.algafoog.auth.domain.Usuario;
@@ -16,8 +17,8 @@ public class AuthUser extends User {
 	private Long userId;
 	private String fullname;
 	
-	public AuthUser(Usuario usuario) {
-		super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
+	public AuthUser(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+		super(usuario.getEmail(), usuario.getSenha(), authorities);
 		
 		this.userId = usuario.getId();
 		this.fullname = usuario.getNome();
--- src/main/java/com/algaworks/algafoog/auth/domain/Permissao.java
+++ src/main/java/com/algaworks/algafoog/auth/domain/Permissao.java
@@ -1,16 +1,10 @@
 package com.algaworks.algafoog.auth.domain;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -19,9 +13,9 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-@Table(name = "usuario")
-public class Usuario {
-
+@Table(name = "permissao")
+public class Permissao {
+	
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,15 +25,5 @@ public class Usuario {
 	private String nome;
 	
 	@Column(nullable = false)
-	private String email;
-	
-	@Column(nullable = false)
-	private String senha;
-	
-	@ManyToMany
-	@JoinTable(name = "usuario_grupo",
-			   joinColumns = @JoinColumn(name = "usuario_id"),
-			   inverseJoinColumns = @JoinColumn(name = "grupo_id"))
-	private Set<Grupo> grupos = new HashSet<>();
-	
+	private String descricao;
 }
