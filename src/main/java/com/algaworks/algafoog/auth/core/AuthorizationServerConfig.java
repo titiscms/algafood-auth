54814538973f99092395cdddedfdc748b096650f
titiscms <titisbackup@gmail.com>
2021-04-18 16:56:04 -0300

    Ajuste no authorization server para buscar configurações de clients no banco de dados.

--- src/main/java/com/algaworks/algafoog/auth/core/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafoog/auth/core/AuthorizationServerConfig.java
@@ -3,13 +3,14 @@ package com.algaworks.algafoog.auth.core;
 import java.security.KeyPair;
 import java.util.Arrays;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -28,12 +29,13 @@ import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFacto
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-	private static final String SCOPE_TYPE_READ = "READ";
-
-	private static final String SCOPE_TYPE_WRITE = "WRITE";
-
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+	/*
+	 * private static final String SCOPE_TYPE_READ = "READ";
+	 * 
+	 * private static final String SCOPE_TYPE_WRITE = "WRITE";
+	 * 
+	 * @Autowired private PasswordEncoder passwordEncoder;
+	 */
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
@@ -43,6 +45,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private JwtKeyStoreProperties jwtKeyStoreProperties;
+	
+	@Autowired
+	private DataSource dataSource;
 
 	/*
 	 * Desabilitado temporariamente
@@ -53,93 +58,96 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients
-			.inMemory()
-				.withClient("algafood-web")
-				.secret(passwordEncoder.encode("web123"))
-				/*
-				 * configuração para usar o fluxo password grant_type + refresh-token
-				 */
-				.authorizedGrantTypes("password", "refresh_token")
-				.scopes(SCOPE_TYPE_WRITE, SCOPE_TYPE_READ)
-				/*
-				 * configuração para definir o tempo de vida do access-token para 1 minutos
-				 * access_token: valor em segundos e padrão é de 12 horas.
-				 */
-				.accessTokenValiditySeconds(60)
-				/*
-				 * configuração para definir o tempo de vida do refresh-token para 3 minutos
-				 * refresh_token: valor em segundos e padrão é de 30 dias.
-				 */
-				.refreshTokenValiditySeconds(60 * 3)
-				
-			.and()
-				.withClient("faturamento")
-				.secret(passwordEncoder.encode("faturamento123"))
-				/*
-				 * configuração para usar o fluxo client_credentials grant_type
-				 * deixando o tempo de vida do access-token padrão 
-				 */
-				.authorizedGrantTypes("client_credentials")
-				.scopes(SCOPE_TYPE_WRITE, SCOPE_TYPE_READ)
-				
-			.and()
-				.withClient("foodanalyticssimple")
-				.secret(passwordEncoder.encode("food123"))
-				/*
-				 * configuração para usar o fluxo authorization_code grant_type
-				 * deixando o tempo de vida do access-token padrão 
-				 */
-				.authorizedGrantTypes("authorization_code")
-				.scopes(SCOPE_TYPE_WRITE, SCOPE_TYPE_READ)
-				.redirectUris("http://www.foodanalytics.local:8082")
-				
-			.and()
-				.withClient("foodanalytics")
-				/*
-				 * configurando para não precisar passar o client_secret como query params
-				 * usando o PKCE 
-				 */
-				.secret(passwordEncoder.encode(""))
-				/*
-				 * configuração para usar o fluxo authorization_code grant_type
-				 * deixando o tempo de vida do access-token padrão 
-				 */
-				.authorizedGrantTypes("authorization_code")
-				.scopes(SCOPE_TYPE_WRITE, SCOPE_TYPE_READ)
-				.redirectUris("http://www.foodanalytics.local:8082")	
-				
-			.and()
-				.withClient("logistica")
-				/*
-				 * configuração para usar o fluxo implicit grant_type
-				 * não requer autenticação do cliente
-				 * deixando o tempo de vida do access-token padrão 
-				 */
-				.authorizedGrantTypes("implicit")
-				.scopes(SCOPE_TYPE_WRITE, SCOPE_TYPE_READ)
-				.redirectUris("http://www.foodlogistics.local:8082")	
-				
-			.and()
-				.withClient("algafood-mobile")
-				.secret(passwordEncoder.encode("mobile123"))
-				.authorizedGrantTypes("password")
-				.scopes(SCOPE_TYPE_WRITE, SCOPE_TYPE_READ)
-				/*
-				 * configuração para definir o tempo de vida do refresh-token para 6 horas
-				 */
-				.accessTokenValiditySeconds(60 * 60 * 6)
-				/*
-				 * configuração para definir o tempo de vida do refresh-token para 60 dias
-				 */
-				.accessTokenValiditySeconds(60 * 60 * 24 * 60)
-				
-			.and()
-				/*
-				 * configuração de acesso do resource server ao authorization server
-				 */
-				.withClient("algafood-check-token")
-				.secret(passwordEncoder.encode("check123"));
+		clients.jdbc(dataSource);
+		
+		// implementação de configuração de clients em memoria.
+//			.inMemory()
+//				.withClient("algafood-web")
+//				.secret(passwordEncoder.encode("web123"))
+//				/*
+//				 * configuração para usar o fluxo password grant_type + refresh-token
+//				 */
+//				.authorizedGrantTypes("password", "refresh_token")
+//				.scopes(SCOPE_TYPE_WRITE, SCOPE_TYPE_READ)
+//				/*
+//				 * configuração para definir o tempo de vida do access-token para 1 minutos
+//				 * access_token: valor em segundos e padrão é de 12 horas.
+//				 */
+//				.accessTokenValiditySeconds(60)
+//				/*
+//				 * configuração para definir o tempo de vida do refresh-token para 3 minutos
+//				 * refresh_token: valor em segundos e padrão é de 30 dias.
+//				 */
+//				.refreshTokenValiditySeconds(60 * 3)
+//				
+//			.and()
+//				.withClient("faturamento")
+//				.secret(passwordEncoder.encode("faturamento123"))
+//				/*
+//				 * configuração para usar o fluxo client_credentials grant_type
+//				 * deixando o tempo de vida do access-token padrão 
+//				 */
+//				.authorizedGrantTypes("client_credentials")
+//				.scopes(SCOPE_TYPE_WRITE, SCOPE_TYPE_READ)
+//				
+//			.and()
+//				.withClient("foodanalyticssimple")
+//				.secret(passwordEncoder.encode("food123"))
+//				/*
+//				 * configuração para usar o fluxo authorization_code grant_type
+//				 * deixando o tempo de vida do access-token padrão 
+//				 */
+//				.authorizedGrantTypes("authorization_code")
+//				.scopes(SCOPE_TYPE_WRITE, SCOPE_TYPE_READ)
+//				.redirectUris("http://www.foodanalytics.local:8082")
+//				
+//			.and()
+//				.withClient("foodanalytics")
+//				/*
+//				 * configurando para não precisar passar o client_secret como query params
+//				 * usando o PKCE 
+//				 */
+//				.secret(passwordEncoder.encode(""))
+//				/*
+//				 * configuração para usar o fluxo authorization_code grant_type
+//				 * deixando o tempo de vida do access-token padrão 
+//				 */
+//				.authorizedGrantTypes("authorization_code")
+//				.scopes(SCOPE_TYPE_WRITE, SCOPE_TYPE_READ)
+//				.redirectUris("http://www.foodanalytics.local:8082")	
+//				
+//			.and()
+//				.withClient("logistica")
+//				/*
+//				 * configuração para usar o fluxo implicit grant_type
+//				 * não requer autenticação do cliente
+//				 * deixando o tempo de vida do access-token padrão 
+//				 */
+//				.authorizedGrantTypes("implicit")
+//				.scopes(SCOPE_TYPE_WRITE, SCOPE_TYPE_READ)
+//				.redirectUris("http://www.foodlogistics.local:8082")	
+//				
+//			.and()
+//				.withClient("algafood-mobile")
+//				.secret(passwordEncoder.encode("mobile123"))
+//				.authorizedGrantTypes("password")
+//				.scopes(SCOPE_TYPE_WRITE, SCOPE_TYPE_READ)
+//				/*
+//				 * configuração para definir o tempo de vida do refresh-token para 6 horas
+//				 */
+//				.accessTokenValiditySeconds(60 * 60 * 6)
+//				/*
+//				 * configuração para definir o tempo de vida do refresh-token para 60 dias
+//				 */
+//				.accessTokenValiditySeconds(60 * 60 * 24 * 60)
+//				
+//			.and()
+//				/*
+//				 * configuração de acesso do resource server ao authorization server
+//				 */
+//				.withClient("algafood-check-token")
+//				.secret(passwordEncoder.encode("check123"));
+		
 	}
 	
 	@Override
